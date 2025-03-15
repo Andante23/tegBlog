18d0fc10228484a3fa4ed606a03ea43a2db74d61
Andante23 <andante1104@naver.com>
2025-03-15 14:57:31 +0900

    meta 태크 삽입

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import sectionDisplay from "./LIB/sectionDisplay.js"; 
-
-
 document.addEventListener("DOMContentLoaded",function(){
 document.getElementById("all-link").addEventListener('click',function(event){
     event.preventDefault(); 
@@ -10,18 +8,12 @@ document.getElementById("post-link").addEventListener('click',function(event){
    event.preventDefault(); 
   sectionDisplay('block','none','none');
 });
-
-
 document.getElementById("home-link").addEventListener('click',function(event){
     event.preventDefault();
     sectionDisplay('none','none','block');
  });
-
-
-
  document.getElementById("about-link").addEventListener('click',function(event){
     event.preventDefault();
     sectionDisplay('none','block','none');
  });
-
 });
